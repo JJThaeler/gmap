663f7ac25dbb2308a9994b4101c7beb6a18de8da
Jordan Thaeler <jjthaeler@gmail.com>
2014-03-26 09:19:32 -0500

    Adding simple gmaps4rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmap::Application.config.secret_key_base = 'c95c750240f2f9053556fb924383aa7758f7300dfbcce18e29700c5b99a631171a721eeceeaf3ddd9237dde7476f6fd05766228ae13c868d32132f0489ddaba0'
